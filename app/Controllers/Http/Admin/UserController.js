239f8f0b22d4002ce3cdc92645a62ad4d286e630
NGY198888 <782413674@qq.com>
2020-08-06 01:03:25 +0800

    提交修改 用户设置的编辑面板是编辑当前用户，这里要重写

--- app/Controllers/Http/Admin/UserController.js
+++ app/Controllers/Http/Admin/UserController.js
@@ -17,18 +17,14 @@ class UserController  extends CrudController{
     console.log('进入user formView');
     let _model=new this.model();
     let _form=await _model.form()
-    let rows=await _model.baseQuery().fetch();
-    let row=rows.rows.length>0?rows.rows[0]:{}
     return {
       fields:_form.fields,
-      row,
+      row:global.request._user,
     }
   }
   async formViewSave({ params, request, response }){
     console.log('进入user formViewSave');
-    let _model=new this.model();
-    let rows=await _model.baseQuery().fetch();
-    let row=rows.rows.length>0?rows.rows[0]:new this.model()
+    let row=global.request._user
     let data =await this.getData(request, row,'Edit');
     await this.validate(response,request,row);
     row.merge(data);
